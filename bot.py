25b6880e4b0240b75d74d3a809a976213de990b9
David458-maker <talarekdawid11@gmail.com>
2024-10-06 11:24:56 +0200

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,32 @@
+import discord
+from discord.ext import commands
+import random
+
+intents = discord.Intents.default()
+intents.members = True
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='#', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'Zalogowaliśmy się jako {bot.user}')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Cześć, jestem bot{bot.user}!')
+
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send("he" * count_heh)
+
+@bot.command()
+async def bye(ctx):
+    await ctx.send(f"bye my friend")
+
+@bot.command()
+async def add(ctx, left: int, right: int):
+    """Adds two numbers together."""
+    await ctx.send(left + right)
+
+bot.run("MTI4OTI2ODI0NzkyMDY0NDE1Nw.Gue3eE.6BwdOMDdJVWU_64tHScBlkZu6NOGmvK8eb9O5g")
